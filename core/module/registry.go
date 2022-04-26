0cf20a690afceb66590c10772bd619d8e8086067
xaxys <tpnnghd@163.com>
2022-04-26 23:39:42 +0800

    Update model register procedure

--- core/module/registry.go
+++ core/module/registry.go
@@ -10,6 +10,7 @@ import (
 	"github.com/xaxys/maintainman/core/rbac"
 	"github.com/xaxys/maintainman/core/router"
 	"github.com/xaxys/maintainman/core/storage"
+	"github.com/xaxys/maintainman/core/util"
 )
 
 type Registry struct {
@@ -28,15 +29,27 @@ func NewRegistry(server *Server) *Registry {
 
 func (r *Registry) Register(module ...*Module) {
 	model := []any{}
+	disabledModule := []*Module{}
+	enabledModule := []*Module{}
+
+	// register module
 	for _, m := range module {
+		// register model
+		model = append(model, m.getModel()...)
+
+		// check and register module
 		configKey := fmt.Sprintf("module.%s", m.ModuleName)
 		if config.AppConfig.Get(configKey) != nil && !config.AppConfig.GetBool(configKey) {
-			logger.Logger.Warnf("module disabled: %s", m.ModuleName)
+			disabledModule = append(disabledModule, m)
 			continue
 		}
+		enabledModule = append(enabledModule, m)
 		r.modules[m.ModuleName] = m
-		model = append(model, m.getModel()...)
+
+		// register permission
 		rbac.RegisterPerm(m.ModuleName, m.ModulePerm)
+
+		// read and update config
 		if m.ModuleConfig != nil {
 			m.ModuleConfig.SetConfigName(m.ModuleName)
 			m.ModuleConfig.SetConfigType("yaml")
@@ -47,22 +60,42 @@ func (r *Registry) Register(module ...*Module) {
 			config.ReadAndUpdateConfig(m.ModuleConfig, m.ModuleName, m.ModuleVersion)
 		}
 	}
+
+	// sync database model
 	database.SyncModel(model...)
-	for _, m := range module {
+
+	// load module
+	for _, m := range enabledModule {
+		// check dependencies
 		for _, dep := range m.ModuleDepends {
 			if r.modules[dep] == nil {
-				logger.Logger.Fatalf("module %s dependencies missing: %s", m.ModuleName, dep)
+				logger.Logger.Fatalf("Module %s dependencies missing: %s", m.ModuleName, dep)
 			}
 		}
+
+		// init module context
 		mctx := &ModuleContext{
 			Server:  r.server,
 			Route:   router.APIRoute.Party(m.ModuleRoute),
 			Storage: storage.InitStorage(m.ModuleConfig),
 			Cache:   cache.InitCache(m.ModuleName, m.ModuleConfig, m.getOnEvict()),
 		}
+
+		// start loading
+		logger.Logger.Debugf("Module Loading: %s", m.ModuleName)
+
+		// load module
 		m.EntryPoint(mctx)
-		logger.Logger.Infof("module loaded: %s", m.ModuleName)
+
+		// finish loading
+		logger.Logger.Debugf("Module Loaded: %s", m.ModuleName)
 	}
+
+	// module log
+	em := util.TransSlice(enabledModule, func(m *Module) string { return m.ModuleName })
+	dm := util.TransSlice(disabledModule, func(m *Module) string { return m.ModuleName })
+	logger.Logger.Infof("%d modules loaded: %v", len(em), em)
+	logger.Logger.Infof("%d modules disabled: %v", len(dm), dm)
 }
 
 func (r *Registry) Get(moduleName string) IModule {
