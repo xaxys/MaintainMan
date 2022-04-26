b77648b012183e2ecd627e3b0a2380bd27bdccc5
xaxys <tpnnghd@163.com>
2022-04-26 16:14:10 +0800

    Update doc_gen script

--- scripts/doc_gen.bat
+++ scripts/doc_gen.bat
@@ -1,3 +1,4 @@
 @echo off
-swag init -d %~dp0\.. -o %~dp0\..\docs\
-redoc-cli bundle %~dp0\..\docs\swagger.json -o %~dp0\..\docs\index.html
+cd %~dp0..
+swag init
+redoc-cli bundle %~dp0..\docs\swagger.json -o %~dp0..\docs\index.html
