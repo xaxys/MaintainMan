930c1a41cbd9e916af607824d4a17ab582bf6e25
xaxys <tpnnghd@163.com>
2023-10-18 16:47:12 +0800

    Update depencies version & Go version
    Signed-off-by: xaxys <tpnnghd@163.com>

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --privileged -e CGO_ENABLED=1 -e GOPROXY=https://goproxy.cn,direct -e GOVERSION=$(go version | awk '{print $3;}') -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/go/src/maintainman -v /usr:/sysroot/usr -w /go/src/maintainman xaxy/goreleaser-cross:v1.18.0 release --snapshot --rm-dist
+docker run --rm --privileged -e CGO_ENABLED=1 -e GOPROXY=https://goproxy.cn,direct -e GOVERSION=$(go version | awk '{print $3;}') -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/go/src/maintainman -v /usr:/sysroot/usr -w /go/src/maintainman goreleaser/goreleaser-cross:v1.21.3 release --snapshot --clean
